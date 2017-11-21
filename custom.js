4b10a2ccaa37f5adb05426278879c0bf5a55db72
[Richard Sabando] <richardsabando@gmail.com>
2017-11-21 08:37:24 -0500

    changes to overall menu-added the menu navigation toggle

--- custom.js
+++ custom.js
@@ -1,4 +1,9 @@
-$(document).ready(function() {
+//$(document).ready(function() {
+
+//**********menu Toggle************
+ $('#showmenu').click(function() {
+                $('.menu').toggle("size");
+        });
 
   $('.open-crawl-menu').on('init reInit afterChange', function (event, slick, currentSlide, nextSlide) {
     //currentSlide is undefined on init -- set it to 0 in this case (currentSlide is 0 based)
@@ -9,8 +14,8 @@ $(document).ready(function() {
     vertical: true,
     infinite: true,
     speed: 250,
-    // prevArrow: $('.slick-prevM'),
-    // nextArrow: $('.slick-nextM'),
+    prevArrow: $('.slick-prevM'),
+    nextArrow: $('.slick-nextM'),
     centerMode: true,
     centerPadding: '220px',
     slidesToShow: 5,
@@ -23,25 +28,28 @@ $(document).ready(function() {
     }
   });
 
-  function getRandom(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  }
-  var canvas = document.getElementById('starfield'),
-  context = canvas.getContext('2d'),
-  stars = 600,
-  colorrange = [0,60,240];
-  canvas.width = window.innerWidth;
-  canvas.height = window.innerHeight;
-  for (var i = 0; i < stars; i++) {
-    var x = Math.random() * canvas.offsetWidth,
-    y = Math.random() * canvas.offsetHeight,
-    radius = Math.random() * 1.2,
-    hue = colorrange[getRandom(0,colorrange.length - 1)],
-    sat = getRandom(50,100);
+  //older code for starfeild below
 
-    context.beginPath();
-    context.arc(x, y, radius, 0, 360);
-    context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
-    context.fill();
-  }
-});
+  // function getRandom(min, max) {
+  //   return Math.floor(Math.random() * (max - min + 1)) + min;
+  // }
+  // var canvas = document.getElementById('starfield'),
+  // context = canvas.getContext('2d'),
+  // stars = 600,
+  // colorrange = [0,60,240];
+  // canvas.width = window.innerWidth;
+  // canvas.height = window.innerHeight;
+  // for (var i = 0; i < stars; i++) {
+  //   var x = Math.random() * canvas.offsetWidth,
+  //   y = Math.random() * canvas.offsetHeight,
+  //   radius = Math.random() * 1.2,
+  //   hue = colorrange[getRandom(0,colorrange.length - 1)],
+  //   sat = getRandom(50,100);
+
+  //   context.beginPath();
+  //   context.arc(x, y, radius, 0, 360);
+  //   context.fillStyle = "hsl(" + hue + ", " + sat + "%, 88%)";
+  //   context.fill();
+  // }
+//});
+
